d8c4ec5e1144bc251424b55728f13246057bcb3c
Denis Balchugov <perfomeria@gmail.com>
2024-02-29 20:52:03 +0700

    Fixed crash while navigation

--- app/src/main/kotlin/com/perfomer/checkielite/navigation/SingleStackScreenContent.kt
+++ app/src/main/kotlin/com/perfomer/checkielite/navigation/SingleStackScreenContent.kt
@@ -3,26 +3,26 @@ package com.perfomer.checkielite.navigation
 import androidx.compose.runtime.Composable
 import cafe.adriel.voyager.core.screen.Screen
 import cafe.adriel.voyager.navigator.Navigator
-import cafe.adriel.voyager.transitions.FadeTransition
-import com.perfomer.checkielite.navigation.voyager.impl.NavigatorHolder
+import cafe.adriel.voyager.transitions.SlideTransition
 import com.perfomer.checkielite.feature.main.navigation.MainScreenProvider
+import com.perfomer.checkielite.navigation.voyager.impl.NavigatorHolder
 
 class SingleStackScreenContent(
-	private val navigatorHolder: NavigatorHolder,
-	private val mainScreenProvider: MainScreenProvider,
+    private val navigatorHolder: NavigatorHolder,
+    private val mainScreenProvider: MainScreenProvider,
 ) {
 
-	@Composable
-	fun Content() {
-		Navigator(
-			screen = mainScreenProvider() as Screen,
-			onBackPressed = { false }
-		) { navigator ->
-			navigatorHolder.navigator = navigator
+    @Composable
+    fun Content() {
+        Navigator(
+            screen = mainScreenProvider() as Screen,
+            onBackPressed = { false }
+        ) { navigator ->
+            navigatorHolder.navigator = navigator
 
-			FadeTransition(navigator) { screen ->
-				screen.Content()
-			}
-		}
-	}
+            SlideTransition(navigator = navigator) { screen ->
+                screen.Content()
+            }
+        }
+    }
 }
--- core/navigation/voyager/src/main/kotlin/com/perfomer/checkielite/navigation/voyager/BaseScreen.kt
+++ core/navigation/voyager/src/main/kotlin/com/perfomer/checkielite/navigation/voyager/BaseScreen.kt
@@ -2,9 +2,13 @@ package com.perfomer.checkielite.navigation.voyager
 
 import androidx.compose.runtime.Composable
 import cafe.adriel.voyager.core.screen.Screen
+import cafe.adriel.voyager.core.screen.ScreenKey
+import cafe.adriel.voyager.core.screen.uniqueScreenKey
 import com.perfomer.checkielite.core.navigation.api.CheckieScreen
 
 abstract class BaseScreen : Screen, CheckieScreen {
+
+	override val key: ScreenKey = uniqueScreenKey
 
 	@Composable
 	final override fun Content() {

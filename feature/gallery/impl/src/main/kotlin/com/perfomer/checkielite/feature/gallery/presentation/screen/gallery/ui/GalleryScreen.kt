d15c30a30819650998a7521b21c1d447cefa237e
Denis Balchugov <perfomeria@gmail.com>
2024-03-03 20:22:46 +0700

    Added PreviewHorizontalPager for Gallery screen

--- feature/gallery/impl/src/main/kotlin/com/perfomer/checkielite/feature/gallery/presentation/screen/gallery/ui/GalleryScreen.kt
+++ feature/gallery/impl/src/main/kotlin/com/perfomer/checkielite/feature/gallery/presentation/screen/gallery/ui/GalleryScreen.kt
@@ -9,10 +9,20 @@ import androidx.compose.animation.fadeIn
 import androidx.compose.animation.fadeOut
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.navigationBarsPadding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.pager.HorizontalPager
+import androidx.compose.foundation.pager.PageSize
+import androidx.compose.foundation.pager.PagerState
 import androidx.compose.foundation.pager.rememberPagerState
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -24,12 +34,19 @@ import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableFloatStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import coil.compose.AsyncImage
 import coil.compose.AsyncImagePainter
 import coil.compose.rememberAsyncImagePainter
 import coil.request.ImageRequest
@@ -40,11 +57,15 @@ import com.mxalbert.zoomable.Zoomable
 import com.mxalbert.zoomable.rememberZoomableState
 import com.perfomer.checkielite.common.ui.CommonDrawable
 import com.perfomer.checkielite.common.ui.cui.effect.UpdateEffect
+import com.perfomer.checkielite.common.ui.cui.modifier.indicatorOffsetForPage
 import com.perfomer.checkielite.common.ui.cui.widget.toolbar.CuiToolbarNavigationIcon
 import com.perfomer.checkielite.common.ui.theme.PreviewTheme
 import com.perfomer.checkielite.common.ui.theme.ScreenPreview
 import com.perfomer.checkielite.feature.gallery.presentation.screen.gallery.ui.state.GalleryUiState
+import kotlinx.collections.immutable.ImmutableList
 import kotlinx.collections.immutable.persistentListOf
+import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.launch
 
 @Composable
 @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
@@ -83,36 +104,96 @@ internal fun GalleryScreen(
         },
         containerColor = GalleryPalette.BackgroundColor.copy(alpha = backgroundAlpha)
     ) {
-        MainHorizontalPager(
-            state = state,
-            onPageChange = onPageChange,
-            onPagerClick = onPagerClick,
-            onDismiss = onDismiss,
-            onDismissProgressChange = { progress -> backgroundAlpha = 1 - progress }
+        Box {
+            val coroutineScope = rememberCoroutineScope()
+            val mainPagerState = rememberPagerState(
+                pageCount = { state.picturesUri.size },
+                initialPage = state.currentPicturePosition,
+            )
+
+            MainHorizontalPager(
+                pagerState = mainPagerState,
+                picturesUri = state.picturesUri,
+                onPageChange = onPageChange,
+                onPagerClick = onPagerClick,
+                onDismissProgressChange = { progress -> backgroundAlpha = 1 - progress },
+                onDismiss = onDismiss,
+            )
+
+            AnimatedVisibility(
+                visible = state.isUiShown, enter = fadeIn(), exit = fadeOut(),
+                modifier = Modifier.align(Alignment.BottomCenter)
+            ) {
+                PreviewHorizontalPager(
+                    mainPagerState = mainPagerState,
+                    picturesUri = state.picturesUri,
+                    onPictureClick = { page ->
+                        coroutineScope.launch { mainPagerState.animateScrollToPage(page) }
+                    },
+                )
+            }
+        }
+    }
+}
+
+@Composable
+private fun PreviewHorizontalPager(
+    mainPagerState: PagerState,
+    picturesUri: ImmutableList<String>,
+    onPictureClick: (position: Int) -> Unit,
+) {
+    val pagerState = rememberPagerState(
+        pageCount = { picturesUri.size },
+        initialPage = mainPagerState.currentPage,
+    )
+
+    UpdateEffect(mainPagerState.currentPage) {
+        coroutineScope { pagerState.animateScrollToPage(mainPagerState.currentPage) }
+    }
+
+    HorizontalPager(
+        state = pagerState,
+        pageSize = PageSize.Fixed(56.dp),
+        pageSpacing = 8.dp,
+        contentPadding = PaddingValues(vertical = 16.dp, horizontal = 24.dp),
+        modifier = Modifier
+            .fillMaxWidth()
+            .background(GalleryPalette.BarColor)
+            .navigationBarsPadding()
+    ) { page ->
+        val offset = mainPagerState.indicatorOffsetForPage(page)
+        val targetAlpha = 0.5F + 0.5F * offset
+
+        AsyncImage(
+            model = picturesUri[page],
+            contentDescription = null,
+            contentScale = ContentScale.Crop,
+            modifier = Modifier
+                .size(56.dp)
+                .clip(RoundedCornerShape(16.dp))
+                .clickable { onPictureClick(page) }
+                .alpha(targetAlpha)
         )
     }
 }
 
 @Composable
 private fun MainHorizontalPager(
-    state: GalleryUiState,
+    pagerState: PagerState,
+    picturesUri: ImmutableList<String>,
     onPageChange: (pageIndex: Int) -> Unit,
     onPagerClick: () -> Unit,
     onDismissProgressChange: (progress: Float) -> Unit,
     onDismiss: () -> Unit,
 ) {
-    val pagerState = rememberPagerState(
-        pageCount = { state.picturesUri.size },
-        initialPage = state.currentPicturePosition,
-    )
-
     UpdateEffect(pagerState.currentPage) { onPageChange(pagerState.currentPage) }
+
     HorizontalPager(
         state = pagerState,
         modifier = Modifier.fillMaxSize()
     ) { page ->
         val zoomableState = rememberZoomableState(
-            minScale = 0.8F,
+            minScale = 0.9F,
             maxScale = 6F,
             overZoomConfig = OverZoomConfig(1F, 4F),
         )
@@ -127,24 +208,7 @@ private fun MainHorizontalPager(
             onDismiss = { onDismiss(); true },
             dismissGestureEnabled = true,
         ) {
-            val painter = rememberAsyncImagePainter(
-                model = ImageRequest.Builder(LocalContext.current)
-                    .data(state.picturesUri[page])
-                    .size(Size.ORIGINAL)
-                    .build()
-            )
-
-            if (painter.state is AsyncImagePainter.State.Success) {
-                val size = painter.intrinsicSize
-
-                Image(
-                    painter = painter,
-                    contentDescription = null,
-                    modifier = Modifier
-                        .aspectRatio(size.width / size.height)
-                        .fillMaxSize()
-                )
-            }
+            MainGalleryPicture(pictureUri = picturesUri[page])
         }
 
         // Reset zoom state when the page is moved out of the window.
@@ -157,6 +221,28 @@ private fun MainHorizontalPager(
                 )
             }
         }
+    }
+}
+
+@Composable
+private fun MainGalleryPicture(pictureUri: String) {
+    val painter = rememberAsyncImagePainter(
+        model = ImageRequest.Builder(LocalContext.current)
+            .data(pictureUri)
+            .size(Size.ORIGINAL)
+            .build()
+    )
+
+    if (painter.state is AsyncImagePainter.State.Success) {
+        val size = painter.intrinsicSize
+
+        Image(
+            painter = painter,
+            contentDescription = null,
+            modifier = Modifier
+                .aspectRatio(size.width / size.height)
+                .fillMaxSize()
+        )
     }
 }
 

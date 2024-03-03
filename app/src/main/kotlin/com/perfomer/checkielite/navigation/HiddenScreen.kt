66018227b160ebed6300173d4bc8da591b93300d
Denis Balchugov <perfomeria@gmail.com>
2024-03-03 15:58:18 +0700

    Fixed Overlay screen transitions

--- app/src/main/kotlin/com/perfomer/checkielite/navigation/HiddenScreen.kt
+++ app/src/main/kotlin/com/perfomer/checkielite/navigation/HiddenScreen.kt
@@ -1,11 +1,9 @@
 package com.perfomer.checkielite.navigation
 
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
 import cafe.adriel.voyager.core.screen.Screen
 import com.perfomer.checkielite.core.navigation.api.NoScreen
 
@@ -13,11 +11,7 @@ internal object HiddenScreen : Screen, NoScreen {
 
     @Composable
     override fun Content() {
-        Spacer(
-            modifier = Modifier
-                .fillMaxWidth()
-                .height(1.dp)
-        )
+        Spacer(Modifier.fillMaxSize())
     }
 
     private fun readResolve(): Any = HiddenScreen

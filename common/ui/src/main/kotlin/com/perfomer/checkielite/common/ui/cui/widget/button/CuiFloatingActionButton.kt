0fc3b2ac228dd0635c9841d3fdc5a74023a76219
Denis Balchugov <perfomeria@gmail.com>
2024-03-05 07:00:02 +0700

    Added elevation animation on FAB press

--- common/ui/src/main/kotlin/com/perfomer/checkielite/common/ui/cui/widget/button/CuiFloatingActionButton.kt
+++ common/ui/src/main/kotlin/com/perfomer/checkielite/common/ui/cui/widget/button/CuiFloatingActionButton.kt
@@ -1,20 +1,35 @@
 package com.perfomer.checkielite.common.ui.cui.widget.button
 
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.FloatingActionButtonDefaults
+import androidx.compose.material3.FloatingActionButtonElevation
 import androidx.compose.material3.Icon
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun CuiFloatingActionButton(
     painter: Painter,
-    contentDescription: String? = null,
     onClick: () -> Unit,
+    modifier: Modifier = Modifier,
+    contentDescription: String? = null,
+    interactionSource: MutableInteractionSource = remember { MutableInteractionSource() },
+    elevation: FloatingActionButtonElevation = FloatingActionButtonDefaults.elevation(
+        defaultElevation = 6.dp,
+        pressedElevation = 0.dp,
+    ),
 ) {
     FloatingActionButton(
         shape = CircleShape,
         onClick = onClick,
+        interactionSource = interactionSource,
+        elevation = elevation,
+        modifier = modifier
     ) {
         Icon(painter = painter, contentDescription = contentDescription)
     }

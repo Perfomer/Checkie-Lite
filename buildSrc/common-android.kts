e8d462d6796a68be2e2df95bccf5389d240cfd16
Denis Balchugov <perfomeria@gmail.com>
2024-03-15 06:39:40 +0700

    Version 1.0.1 hotfix released

--- buildSrc/common-android.kts
+++ buildSrc/common-android.kts
@@ -5,8 +5,8 @@ android {
         targetSdk = 34
         minSdk = 24
 
-        versionCode = 1
-        versionName = "1.0.0"
+        versionCode = 2
+        versionName = "1.0.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables.useSupportLibrary = true

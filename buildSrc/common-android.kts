0737738d0561f84aa48434587a3e48fbf9c1cb5f
Denis Balchugov <perfomeria@gmail.com>
2024-04-07 00:12:55 +0700

    Version 1.2.0 released

--- buildSrc/common-android.kts
+++ buildSrc/common-android.kts
@@ -5,8 +5,8 @@ android {
         targetSdk = 34
         minSdk = 24
 
-        versionCode = 3
-        versionName = "1.1.0"
+        versionCode = 4
+        versionName = "1.2.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables.useSupportLibrary = true

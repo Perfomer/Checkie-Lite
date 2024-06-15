5953124e4153ded2d30eef6d28b9bc19e7222646
Denis Balchugov <perfomeria@gmail.com>
2024-06-15 22:52:24 +0700

    Added CuiToast for TagCreation fails

---
+++ common/ui/src/main/kotlin/com/perfomer/checkielite/common/ui/cui/widget/toast/CuiToast.kt
@@ -0,0 +1,62 @@
+package com.perfomer.checkielite.common.ui.cui.widget.toast
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.perfomer.checkielite.common.ui.theme.LocalCuiPalette
+
+@Composable
+fun CuiToast(
+    data: CuiToastData,
+    onClick: () -> Unit,
+) {
+    Row(
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.Center,
+        modifier = Modifier
+            .padding(top = 40.dp)
+            .shadow(
+                elevation = 20.dp,
+                spotColor = Color.Black.copy(alpha = 0.7F),
+                shape = CircleShape,
+            )
+            .background(color = LocalCuiPalette.current.BackgroundNegativeSecondary)
+            .clip(CircleShape)
+            .clickable(onClick = onClick)
+            .padding(vertical = 13.dp)
+            .padding(end = 24.dp, start = 16.dp)
+    ) {
+        Icon(
+            painter = data.icon,
+            tint = LocalCuiPalette.current.IconNegative,
+            contentDescription = null,
+            modifier = Modifier.size(20.dp)
+        )
+
+        Spacer(modifier = Modifier.width(16.dp))
+
+        Text(
+            text = data.message,
+            color = LocalCuiPalette.current.TextPrimary,
+            fontSize = 14.sp,
+            fontWeight = FontWeight.Medium
+        )
+    }
+}
---
+++ common/ui/src/main/kotlin/com/perfomer/checkielite/common/ui/cui/widget/toast/CuiToastHost.kt
@@ -0,0 +1,112 @@
+package com.perfomer.checkielite.common.ui.cui.widget.toast
+
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.slideInVertically
+import androidx.compose.animation.slideOutVertically
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.Stable
+import androidx.compose.runtime.compositionLocalOf
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import com.perfomer.checkielite.common.ui.cui.widget.toast.CuiToastHostState.Companion.DEFAULT_DURATION_MS
+import kotlinx.coroutines.delay
+
+val LocalCuiToastHostState = compositionLocalOf<CuiToastHostState> { error("Not provided") }
+
+@Stable
+class CuiToastHostState {
+
+    var currentToastData by mutableStateOf<CuiToastData?>(null)
+        private set
+
+    var show by mutableStateOf(false)
+        private set
+
+
+    fun showToast(
+        message: String,
+        icon: Painter,
+        durationMs: Long = DEFAULT_DURATION_MS,
+    ) {
+        showToast(CuiToastData(message, icon, durationMs))
+    }
+
+    fun showToast(data: CuiToastData) {
+        currentToastData = data
+        show = true
+    }
+
+    fun dismiss() {
+        show = false
+    }
+
+    internal companion object {
+        internal const val DEFAULT_DURATION_MS = 5_000L
+    }
+}
+
+@Composable
+fun rememberToast(
+    @StringRes message: Int,
+    @DrawableRes icon: Int,
+    durationMs: Long = DEFAULT_DURATION_MS,
+): CuiToastData {
+    val actualMessage = stringResource(message)
+    val actualIcon = painterResource(icon)
+
+    return remember(message, icon) {
+        CuiToastData(actualMessage, actualIcon, durationMs)
+    }
+}
+
+@Stable
+data class CuiToastData(
+    val message: String,
+    val icon: Painter,
+    val durationMs: Long,
+)
+
+@Composable
+fun CuiToastHost(
+    hostState: CuiToastHostState = LocalCuiToastHostState.current,
+    toast: @Composable (CuiToastData) -> Unit = { data ->
+        CuiToast(
+            data = data,
+            onClick = { hostState.dismiss() },
+        )
+    },
+) {
+    val currentToastData = hostState.currentToastData
+
+    LaunchedEffect(hostState.show) {
+        if (currentToastData != null && hostState.show) {
+            delay(currentToastData.durationMs)
+            hostState.dismiss()
+        }
+    }
+
+    Box(
+        contentAlignment = Alignment.TopCenter,
+        modifier = Modifier.fillMaxSize()
+    ) {
+        AnimatedVisibility(
+            visible = hostState.show && currentToastData != null,
+            enter = slideInVertically(),
+            exit = slideOutVertically(targetOffsetY = { -it }),
+        ) {
+            toast(currentToastData!!)
+        }
+    }
+}

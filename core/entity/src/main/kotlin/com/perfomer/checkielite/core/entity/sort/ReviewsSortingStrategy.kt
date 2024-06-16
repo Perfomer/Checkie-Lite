78394810080f30abbb6b5e4115b1febfb6eaa2d9
Denis Balchugov <perfomeria@gmail.com>
2024-06-16 13:20:54 +0700

    Changed order of sorting options

--- core/entity/src/main/kotlin/com/perfomer/checkielite/core/entity/sort/ReviewsSortingStrategy.kt
+++ core/entity/src/main/kotlin/com/perfomer/checkielite/core/entity/sort/ReviewsSortingStrategy.kt
@@ -2,8 +2,8 @@ package com.perfomer.checkielite.core.entity.sort
 
 enum class ReviewsSortingStrategy {
     RELEVANCE,
-    NEWEST,
-    OLDEST,
     MOST_RATED,
     LEAST_RATED,
+    NEWEST,
+    OLDEST,
 }

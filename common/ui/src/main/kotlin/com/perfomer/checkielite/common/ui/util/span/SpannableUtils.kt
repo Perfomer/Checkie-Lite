976ce108b35bd1b9453716a48b6584fc929b537e
Denis Balchugov <perfomeria@gmail.com>
2024-07-20 15:27:22 +0700

    Fixed design for BackupScreen

--- common/ui/src/main/kotlin/com/perfomer/checkielite/common/ui/util/span/SpannableUtils.kt
+++ common/ui/src/main/kotlin/com/perfomer/checkielite/common/ui/util/span/SpannableUtils.kt
@@ -1,102 +1,188 @@
 package com.perfomer.checkielite.common.ui.util.span
 
+import android.content.res.Resources
+import android.graphics.Typeface
+import android.text.Spanned
+import android.text.SpannedString
+import android.text.style.AbsoluteSizeSpan
+import android.text.style.BulletSpan
+import android.text.style.ForegroundColorSpan
+import android.text.style.RelativeSizeSpan
+import android.text.style.StrikethroughSpan
+import android.text.style.StyleSpan
+import android.text.style.SubscriptSpan
+import android.text.style.SuperscriptSpan
+import android.text.style.TypefaceSpan
+import android.text.style.UnderlineSpan
+import android.util.Log
+import androidx.annotation.StringRes
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.ReadOnlyComposable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalConfiguration
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.text.AnnotatedString
 import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.BaselineShift
 import androidx.compose.ui.text.style.TextDecoration
-import kotlin.math.min
+import androidx.compose.ui.unit.Density
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.em
+import androidx.core.text.HtmlCompat
 
-/**
- * The tags to interpret. Add tags here and in [tagToStyle].
- */
-private val tags = linkedMapOf(
-    "<b>" to "</b>",
-    "<i>" to "</i>",
-    "<u>" to "</u>"
-)
+@Composable
+@ReadOnlyComposable
+private fun resources(): Resources {
+    LocalConfiguration.current
+    return LocalContext.current.resources
+}
 
-/**
- * The main entry point. Call this on a String and use the result in a Text.
- */
-fun String.parseHtml(): AnnotatedString {
-    val newlineReplace = this.replace("<br>", "\n")
+fun Spanned.toHtmlWithoutParagraphs(): String {
+    return HtmlCompat.toHtml(this, HtmlCompat.TO_HTML_PARAGRAPH_LINES_CONSECUTIVE)
+        .substringAfter("<p dir=\"ltr\">").substringBeforeLast("</p>")
+}
 
-    return buildAnnotatedString {
-        recurse(newlineReplace, this)
+fun Resources.getText(@StringRes id: Int, vararg args: Any): CharSequence {
+    val escapedArgs = args.map {
+        if (it is Spanned) it.toHtmlWithoutParagraphs() else it
+    }.toTypedArray()
+    val resource = SpannedString(getText(id))
+    val htmlResource = resource.toHtmlWithoutParagraphs()
+    val formattedHtml = String.format(htmlResource, *escapedArgs)
+    return HtmlCompat.fromHtml(formattedHtml, HtmlCompat.FROM_HTML_MODE_LEGACY)
+}
+
+@Composable
+fun annotatedStringResource(@StringRes id: Int, vararg formatArgs: Any): AnnotatedString {
+    val resources = resources()
+    val density = LocalDensity.current
+    return remember(id, formatArgs) {
+        val text = resources.getText(id, *formatArgs)
+        spannableStringToAnnotatedString(text, density)
     }
 }
 
-/**
- * Recurses through the given HTML String to convert it to an AnnotatedString.
- *
- * @param string the String to examine.
- * @param to the AnnotatedString to append to.
- */
-private fun recurse(string: String, to: AnnotatedString.Builder) {
-    //Find the opening tag that the given String starts with, if any.
-    val startTag = tags.keys.find { string.startsWith(it) }
+@Composable
+fun annotatedStringResource(@StringRes id: Int): AnnotatedString {
+    val resources = resources()
+    val density = LocalDensity.current
+    return remember(id) {
+        val text = resources.getText(id)
+        spannableStringToAnnotatedString(text, density)
+    }
+}
 
-    //Find the closing tag that the given String starts with, if any.
-    val endTag = tags.values.find { string.startsWith(it) }
+private fun spannableStringToAnnotatedString(
+    text: CharSequence,
+    density: Density
+): AnnotatedString {
+    return if (text is Spanned) {
+        with(density) {
+            buildAnnotatedString {
+                append((text.toString()))
+                text.getSpans(0, text.length, Any::class.java).forEach {
+                    val start = text.getSpanStart(it)
+                    val end = text.getSpanEnd(it)
+                    when (it) {
+                        is StyleSpan -> when (it.style) {
+                            Typeface.NORMAL -> addStyle(
+                                SpanStyle(
+                                    fontWeight = FontWeight.Normal,
+                                    fontStyle = FontStyle.Normal
+                                ),
+                                start,
+                                end
+                            )
+                            Typeface.BOLD -> addStyle(
+                                SpanStyle(
+                                    fontWeight = FontWeight.Bold,
+                                    fontStyle = FontStyle.Normal
+                                ),
+                                start,
+                                end
+                            )
+                            Typeface.ITALIC -> addStyle(
+                                SpanStyle(
+                                    fontWeight = FontWeight.Normal,
+                                    fontStyle = FontStyle.Italic
+                                ),
+                                start,
+                                end
+                            )
+                            Typeface.BOLD_ITALIC -> addStyle(
+                                SpanStyle(
+                                    fontWeight = FontWeight.Bold,
+                                    fontStyle = FontStyle.Italic
+                                ),
+                                start,
+                                end
+                            )
+                        }
+                        is TypefaceSpan -> addStyle(
+                            SpanStyle(
+                                fontFamily = when (it.family) {
+                                    FontFamily.SansSerif.name -> FontFamily.SansSerif
+                                    FontFamily.Serif.name -> FontFamily.Serif
+                                    FontFamily.Monospace.name -> FontFamily.Monospace
+                                    FontFamily.Cursive.name -> FontFamily.Cursive
+                                    else -> FontFamily.Default
+                                }
+                            ),
+                            start,
 
-    when {
-        //If the String starts with a closing tag, then pop the latest-applied
-        //SpanStyle and continue recursing.
-        tags.any { string.startsWith(it.value) } -> {
-            to.pop()
-            recurse(string.removeRange(0, endTag!!.length), to)
-        }
-        //If the String starts with an opening tag, apply the appropriate
-        //SpanStyle and continue recursing.
-        tags.any { string.startsWith(it.key) } -> {
-            to.pushStyle(tagToStyle(startTag!!))
-            recurse(string.removeRange(0, startTag.length), to)
-        }
-        //If the String doesn't start with an opening or closing tag, but does contain either,
-        //find the lowest index (that isn't -1/not found) for either an opening or closing tag.
-        //Append the text normally up until that lowest index, and then recurse starting from that index.
-        tags.any { string.contains(it.key) || string.contains(it.value) } -> {
-            val firstStart = tags.keys.map { string.indexOf(it) }.filterNot { it == -1 }.minOrNull() ?: -1
-            val firstEnd = tags.values.map { string.indexOf(it) }.filterNot { it == -1 }.minOrNull() ?: -1
-            val first = when {
-                firstStart == -1 -> firstEnd
-                firstEnd == -1 -> firstStart
-                else -> min(firstStart, firstEnd)
+                            end
+                        )
+                        is BulletSpan -> {
+                            Log.d("StringResources", "BulletSpan not supported yet")
+                            addStyle(SpanStyle(), start, end)
+                        }
+                        is AbsoluteSizeSpan -> addStyle(
+                            SpanStyle(fontSize = if (it.dip) it.size.dp.toSp() else it.size.toSp()),
+                            start,
+                            end
+                        )
+                        is RelativeSizeSpan -> addStyle(
+                            SpanStyle(fontSize = it.sizeChange.em),
+                            start,
+                            end
+                        )
+                        is StrikethroughSpan -> addStyle(
+                            SpanStyle(textDecoration = TextDecoration.LineThrough),
+                            start,
+                            end
+                        )
+                        is UnderlineSpan -> addStyle(
+                            SpanStyle(textDecoration = TextDecoration.Underline),
+                            start,
+                            end
+                        )
+                        is SuperscriptSpan -> addStyle(
+                            SpanStyle(baselineShift = BaselineShift.Superscript),
+                            start,
+                            end
+                        )
+                        is SubscriptSpan -> addStyle(
+                            SpanStyle(baselineShift = BaselineShift.Subscript),
+                            start,
+                            end
+                        )
+                        is ForegroundColorSpan -> addStyle(
+                            SpanStyle(color = Color(it.foregroundColor)),
+                            start,
+                            end
+                        )
+                        else -> addStyle(SpanStyle(), start, end)
+                    }
+                }
             }
-
-            to.append(string.substring(0, first))
-
-            recurse(string.removeRange(0, first), to)
         }
-        //There weren't any supported tags found in the text. Just append it all normally.
-        else -> {
-            to.append(string)
-        }
-    }
-}
-
-/**
- * Get a [SpanStyle] for a given (opening) tag.
- * Add your own tag styling here by adding its opening tag to
- * the when clause and then instantiating the appropriate [SpanStyle].
- *
- * @return a [SpanStyle] for the given tag.
- */
-private fun tagToStyle(tag: String): SpanStyle {
-    return when (tag) {
-        "<b>" -> {
-            SpanStyle(fontWeight = FontWeight.Bold)
-        }
-        "<i>" -> {
-            SpanStyle(fontStyle = FontStyle.Italic)
-        }
-        "<u>" -> {
-            SpanStyle(textDecoration = TextDecoration.Underline)
-        }
-        //This should only throw if you add a tag to the [tags] Map and forget to add it
-        //to this function.
-        else -> throw IllegalArgumentException("Tag $tag is not valid.")
+    } else {
+        AnnotatedString(text.toString())
     }
 }

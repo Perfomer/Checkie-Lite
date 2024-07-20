80f9ea383065a9a256504424caed0d2c64907c05
Denis Balchugov <perfomeria@gmail.com>
2024-07-20 15:28:42 +0700

    Removed unnecessary currencies symbols

--- core/entity/src/main/kotlin/com/perfomer/checkielite/core/entity/price/CurrencySymbol.kt
+++ core/entity/src/main/kotlin/com/perfomer/checkielite/core/entity/price/CurrencySymbol.kt
@@ -4,27 +4,17 @@ object CurrencySymbol {
 
     private val symbols: Map<String, String> = mapOf(
         "AMD" to "֏",
-        "ARS" to "$",
-        "AUD" to "$",
         "AZN" to "₼",
-        "BND" to "$",
-        "CAD" to "$",
-        "CLP" to "$",
-        "COP" to "$",
-        "CUP" to "$",
-        "DOP" to "$",
         "GEL" to "₾",
         "GHS" to "₵",
         "KZT" to "₸",
         "LAK" to "₭",
         "MNT" to "₮",
-        "MXN" to "$",
         "NGN" to "₦",
         "PHP" to "₱",
         "PYG" to "₲",
         "TRY" to "₺",
         "UAH" to "₴",
-        "UYU" to "$",
     )
 
     fun getSymbol(currencyCode: String): String? {

ab0c3286dfa2f6152a6934ab5520f1ee8be10145
Denis Balchugov <perfomeria@gmail.com>
2024-10-12 17:34:32 +0700

    Version 1.5.1 released

--- buildSrc/common-android.kts
+++ buildSrc/common-android.kts
@@ -5,8 +5,8 @@ android {
         targetSdk = 34
         minSdk = 24
 
-        versionCode = 7
-        versionName = "1.5.0"
+        versionCode = 8
+        versionName = "1.5.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables.useSupportLibrary = true

c17c37af9ab1dd2554a5db890b5c31e7503a6e4a
Denis Balchugov <perfomeria@gmail.com>
2024-10-13 16:19:56 +0700

    Added more variants for toasts

--- common/ui/src/main/kotlin/com/perfomer/checkielite/common/ui/cui/widget/toast/CuiToastHost.kt
+++ common/ui/src/main/kotlin/com/perfomer/checkielite/common/ui/cui/widget/toast/CuiToastHost.kt
@@ -17,10 +17,13 @@ import androidx.compose.runtime.setValue
 import androidx.compose.runtime.staticCompositionLocalOf
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import com.perfomer.checkielite.common.ui.CommonDrawable
 import com.perfomer.checkielite.common.ui.cui.widget.toast.CuiToastHostState.Companion.DEFAULT_DURATION_MS
+import com.perfomer.checkielite.common.ui.theme.LocalCuiPalette
 import kotlinx.coroutines.delay
 
 val LocalCuiToastHostState = staticCompositionLocalOf<CuiToastHostState> { error("Not provided") }
@@ -33,15 +36,6 @@ class CuiToastHostState {
 
     var show by mutableStateOf(false)
         private set
-
-
-    fun showToast(
-        message: String,
-        icon: Painter,
-        durationMs: Long = DEFAULT_DURATION_MS,
-    ) {
-        showToast(CuiToastData(message, icon, durationMs))
-    }
 
     fun showToast(data: CuiToastData) {
         currentToastData = data
@@ -60,21 +54,53 @@ class CuiToastHostState {
 @Composable
 fun rememberToast(
     @StringRes message: Int,
-    @DrawableRes icon: Int,
+    @DrawableRes icon: Int? = null,
+    iconTint: Color = Color.Unspecified,
+    backgroundColor: Color = Color.Unspecified,
     durationMs: Long = DEFAULT_DURATION_MS,
 ): CuiToastData {
     val actualMessage = stringResource(message)
-    val actualIcon = painterResource(icon)
+    val actualIcon = icon?.let { painterResource(it) }
 
-    return remember(message, icon) {
-        CuiToastData(actualMessage, actualIcon, durationMs)
+    return remember(message, icon, iconTint, backgroundColor) {
+        CuiToastData(actualMessage, actualIcon, iconTint, backgroundColor, durationMs)
     }
+}
+
+@Composable
+fun rememberSuccessToast(
+    @StringRes message: Int,
+    durationMs: Long = DEFAULT_DURATION_MS,
+): CuiToastData {
+    return rememberToast(
+        message = message,
+        icon = CommonDrawable.ic_success,
+        iconTint = LocalCuiPalette.current.IconPositive,
+        backgroundColor = LocalCuiPalette.current.BackgroundPositiveSecondary,
+        durationMs = durationMs,
+    )
+}
+
+@Composable
+fun rememberErrorToast(
+    @StringRes message: Int,
+    durationMs: Long = DEFAULT_DURATION_MS,
+): CuiToastData {
+    return rememberToast(
+        message = message,
+        icon = CommonDrawable.ic_error,
+        iconTint = LocalCuiPalette.current.IconNegative,
+        backgroundColor = LocalCuiPalette.current.BackgroundNegativeSecondary,
+        durationMs = durationMs,
+    )
 }
 
 @Stable
 data class CuiToastData(
     val message: String,
-    val icon: Painter,
+    val icon: Painter?,
+    val iconTint: Color,
+    val backgroundColor: Color,
     val durationMs: Long,
 )
 

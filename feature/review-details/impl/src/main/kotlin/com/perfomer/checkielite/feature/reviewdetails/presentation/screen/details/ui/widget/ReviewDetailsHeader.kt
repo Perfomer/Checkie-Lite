bbf11c6d716fd8cee339b3b5dfeb965bfb399e77
Denis Balchugov <perfomeria@gmail.com>
2024-10-20 00:53:16 +0700

    Allowed to select text in comment or title of Review Details

--- feature/review-details/impl/src/main/kotlin/com/perfomer/checkielite/feature/reviewdetails/presentation/screen/details/ui/widget/ReviewDetailsHeader.kt
+++ feature/review-details/impl/src/main/kotlin/com/perfomer/checkielite/feature/reviewdetails/presentation/screen/details/ui/widget/ReviewDetailsHeader.kt
@@ -1,8 +1,10 @@
 package com.perfomer.checkielite.feature.reviewdetails.presentation.screen.details.ui.widget
 
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.selection.SelectionContainer
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -16,22 +18,26 @@ internal fun ReviewDetailsHeader(
     productName: String,
     brandName: String?,
 ) {
-    if (brandName != null) {
-        Text(
-            text = brandName,
-            color = LocalCuiPalette.current.TextAccent,
-            fontWeight = FontWeight.Bold,
-            fontSize = 14.sp,
-            modifier = Modifier.padding(horizontal = 24.dp),
-        )
+    SelectionContainer {
+        Column {
+            if (brandName != null) {
+                Text(
+                    text = brandName,
+                    color = LocalCuiPalette.current.TextAccent,
+                    fontWeight = FontWeight.Bold,
+                    fontSize = 14.sp,
+                    modifier = Modifier.padding(horizontal = 24.dp),
+                )
 
-        Spacer(Modifier.height(8.dp))
+                Spacer(Modifier.height(8.dp))
+            }
+
+            Text(
+                text = productName,
+                fontSize = 24.sp,
+                fontWeight = FontWeight.Bold,
+                modifier = Modifier.padding(horizontal = 24.dp)
+            )
+        }
     }
-
-    Text(
-        text = productName,
-        fontSize = 24.sp,
-        fontWeight = FontWeight.Bold,
-        modifier = Modifier.padding(horizontal = 24.dp)
-    )
 }

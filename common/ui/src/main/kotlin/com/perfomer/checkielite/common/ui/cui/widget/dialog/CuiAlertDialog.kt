fe77ac0cfe1238a7bb1470b87a819f0dfd06b427
Denis Balchugov <perfomeria@gmail.com>
2024-10-31 00:39:43 +0700

    Added CuiAlertDialog with AnnotatedString

--- common/ui/src/main/kotlin/com/perfomer/checkielite/common/ui/cui/widget/dialog/CuiAlertDialog.kt
+++ common/ui/src/main/kotlin/com/perfomer/checkielite/common/ui/cui/widget/dialog/CuiAlertDialog.kt
@@ -6,6 +6,7 @@ import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.AnnotatedString
 import androidx.compose.ui.unit.dp
 import com.perfomer.checkielite.common.ui.CommonString
 import com.perfomer.checkielite.common.ui.theme.LocalCuiPalette
@@ -15,10 +16,56 @@ fun CuiAlertDialog(
     title: String,
     text: String,
     confirmText: String = stringResource(CommonString.common_ok),
-    confirmColor: Color = Color.Unspecified,
+    confirmColor: Color = LocalCuiPalette.current.TextAccent,
     onConfirm: () -> Unit,
     dismissText: String? = stringResource(CommonString.common_cancel),
-    dismissColor: Color = Color.Unspecified,
+    dismissColor: Color = LocalCuiPalette.current.TextAccent,
+    onDismiss: () -> Unit,
+) {
+    CuiAlertDialogInternal(
+        title = title,
+        text = { Text(text) },
+        confirmText = confirmText,
+        confirmColor = confirmColor,
+        onConfirm = onConfirm,
+        dismissText = dismissText,
+        dismissColor = dismissColor,
+        onDismiss = onDismiss,
+    )
+}
+
+@Composable
+fun CuiAlertDialog(
+    title: String,
+    text: AnnotatedString,
+    confirmText: String = stringResource(CommonString.common_ok),
+    confirmColor: Color = LocalCuiPalette.current.TextAccent,
+    onConfirm: () -> Unit,
+    dismissText: String? = stringResource(CommonString.common_cancel),
+    dismissColor: Color = LocalCuiPalette.current.TextAccent,
+    onDismiss: () -> Unit,
+) {
+    CuiAlertDialogInternal(
+        title = title,
+        text = { Text(text) },
+        confirmText = confirmText,
+        confirmColor = confirmColor,
+        onConfirm = onConfirm,
+        dismissText = dismissText,
+        dismissColor = dismissColor,
+        onDismiss = onDismiss,
+    )
+}
+
+@Composable
+private fun CuiAlertDialogInternal(
+    title: String,
+    text: @Composable () -> Unit,
+    confirmText: String,
+    confirmColor: Color,
+    onConfirm: () -> Unit,
+    dismissText: String?,
+    dismissColor: Color,
     onDismiss: () -> Unit,
 ) {
     AlertDialog(
@@ -29,12 +76,7 @@ fun CuiAlertDialog(
                 color = LocalCuiPalette.current.TextPrimary,
             )
         },
-        text = {
-            Text(
-                text = text,
-                color = LocalCuiPalette.current.TextSecondary,
-            )
-        },
+        text = text,
         confirmButton = {
             TextButton(
                 onClick = {

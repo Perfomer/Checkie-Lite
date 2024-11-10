e4d255a16f0b2cd016e5dadc4c145d3fbac40cd3
Denis Balchugov <perfomeria@gmail.com>
2024-11-10 15:14:04 +0700

    Create screen with DI

--- app/src/main/kotlin/com/perfomer/checkielite/newnavigation/screenb/tea/ScrenBDi.kt
+++ app/src/main/kotlin/com/perfomer/checkielite/newnavigation/screenb/tea/ScrenBDi.kt
@@ -2,12 +2,15 @@ package com.perfomer.checkielite.newnavigation.screenb.tea
 
 import com.arkivanov.decompose.ComponentContext
 import com.perfomer.checkielite.newnavigation.BDestination
+import com.perfomer.checkielite.newnavigation.screenb.ScreenB
 import org.koin.core.module.dsl.factoryOf
 import org.koin.dsl.module
 
 val screenBModule = module {
+    factory { params -> ScreenB(get { params }) }
     factoryOf(::createScreenBStore)
 }
+
 
 internal fun createScreenBStore(
     componentContext: ComponentContext,

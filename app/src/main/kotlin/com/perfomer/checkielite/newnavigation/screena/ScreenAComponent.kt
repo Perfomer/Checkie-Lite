de07bc77d170c459b2b40e43c74544ad6d5a3a59
Denis Balchugov <perfomeria@gmail.com>
2024-11-11 07:33:47 +0700

    Abstract screens

--- app/src/main/kotlin/com/perfomer/checkielite/newnavigation/screena/ScreenAComponent.kt
+++ app/src/main/kotlin/com/perfomer/checkielite/newnavigation/screena/ScreenAComponent.kt
@@ -1,12 +1,16 @@
 package com.perfomer.checkielite.newnavigation.screena
 
 import com.arkivanov.decompose.ComponentContext
+import com.arkivanov.decompose.router.stack.StackNavigation
+import com.arkivanov.decompose.router.stack.pushNew
 import com.arkivanov.decompose.value.MutableValue
 import com.arkivanov.decompose.value.Value
+import com.perfomer.checkielite.newnavigation.BDestination
+import com.perfomer.checkielite.newnavigation.Destination
 
 class ScreenAComponent(
     componentContext: ComponentContext,
-    private val onNavigateToScreenB: (String) -> Unit
+    private val navigation: StackNavigation<Destination>
 ): ComponentContext by componentContext {
 
     private var _text = MutableValue("")
@@ -14,7 +18,9 @@ class ScreenAComponent(
 
     fun onEvent(event: ScreenAEvent) {
         when(event) {
-            ScreenAEvent.ClickButtonA -> onNavigateToScreenB(text.value)
+            ScreenAEvent.ClickButtonA -> {
+                navigation.pushNew(BDestination(text.value))
+            }
             is ScreenAEvent.UpdateText -> {
                 _text.value = event.text
             }
--- app/src/main/kotlin/com/perfomer/checkielite/newnavigation/screenb/tea/ScrenBDi.kt
+++ app/src/main/kotlin/com/perfomer/checkielite/newnavigation/screenb/tea/ScrenBDi.kt
@@ -1,16 +1,24 @@
 package com.perfomer.checkielite.newnavigation.screenb.tea
 
 import com.arkivanov.decompose.ComponentContext
+import com.arkivanov.decompose.router.stack.StackNavigation
 import com.perfomer.checkielite.newnavigation.BDestination
+import com.perfomer.checkielite.newnavigation.Destination
+import com.perfomer.checkielite.newnavigation.screena.ScreenA
+import com.perfomer.checkielite.newnavigation.screena.ScreenAComponent
 import com.perfomer.checkielite.newnavigation.screenb.ScreenB
 import org.koin.core.module.dsl.factoryOf
 import org.koin.dsl.module
 
-val screenBModule = module {
-    factory { params -> ScreenB(get { params }) }
+val decomposeModule = module {
+    single { StackNavigation<Destination>() }
+
+    factoryOf(::ScreenA)
+    factoryOf(::ScreenB)
+
+    factoryOf(::ScreenAComponent)
     factoryOf(::createScreenBStore)
 }
-
 
 internal fun createScreenBStore(
     componentContext: ComponentContext,

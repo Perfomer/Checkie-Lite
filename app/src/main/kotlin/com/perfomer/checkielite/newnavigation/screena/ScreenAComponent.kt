fbed2dc903223454406fac215d2a5895bef43801
Denis Balchugov <perfomeria@gmail.com>
2024-11-14 21:29:10 +0700

    Navigation result

--- app/src/main/kotlin/com/perfomer/checkielite/newnavigation/screena/ScreenAComponent.kt
+++ app/src/main/kotlin/com/perfomer/checkielite/newnavigation/screena/ScreenAComponent.kt
@@ -3,21 +3,34 @@ package com.perfomer.checkielite.newnavigation.screena
 import com.arkivanov.decompose.ComponentContext
 import com.arkivanov.decompose.value.MutableValue
 import com.arkivanov.decompose.value.Value
+import com.perfomer.checkielite.core.navigation.DestinationMode
 import com.perfomer.checkielite.core.navigation.Router
 import com.perfomer.checkielite.newnavigation.BDestination
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 class ScreenAComponent(
     componentContext: ComponentContext,
     private val router: Router,
 ) : ComponentContext by componentContext {
 
+    private val scope = CoroutineScope(Dispatchers.Main.immediate)
     private var _text = MutableValue("")
     val text: Value<String> = _text
 
     fun onEvent(event: ScreenAEvent) {
         when (event) {
             ScreenAEvent.ClickButtonA -> {
-                router.navigate(BDestination(text.value))
+                scope.launch {
+                    val result = router.navigateForResult<String>(
+                        BDestination(text.value),
+                        DestinationMode.BOTTOM_SHEET,
+                    )
+
+                    _text.value = result
+                }
+//                router.navigate(BDestination(text.value))
             }
             is ScreenAEvent.UpdateText -> {
                 _text.value = event.text

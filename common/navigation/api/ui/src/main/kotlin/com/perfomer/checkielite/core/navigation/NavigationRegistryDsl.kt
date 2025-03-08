137db1765ab48c8c02d508cbe34b46618184633c
Denis Balchugov <perfomeria@gmail.com>
2025-03-09 00:00:28 +0700

    Add DSL marker for Navigation

--- common/navigation/api/ui/src/main/kotlin/com/perfomer/checkielite/core/navigation/NavigationRegistryDsl.kt
+++ common/navigation/api/ui/src/main/kotlin/com/perfomer/checkielite/core/navigation/NavigationRegistryDsl.kt
@@ -2,8 +2,13 @@ package com.perfomer.checkielite.core.navigation
 
 import kotlinx.serialization.serializer
 
+@DslMarker
+annotation class NavigationDsl
+
+@NavigationDsl
 fun navigation(block: NavigationRegistry.() -> Unit) = NavigationRegistry.apply(block)
 
+@NavigationDsl
 inline fun <reified D : Destination, reified S : Screen> NavigationRegistry.associate() {
     register(
         destinationClass = D::class,

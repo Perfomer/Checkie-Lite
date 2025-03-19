452822f23e0adc8828c609455245b35caf933c60
Denis Balchugov <perfomeria@gmail.com>
2025-03-19 22:45:27 +0700

    Make PredictiveBack animation more smooth for OverlayRoot

--- app/src/main/kotlin/com/perfomer/checkielite/AppActivity.kt
+++ app/src/main/kotlin/com/perfomer/checkielite/AppActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.foundation.layout.Box
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
@@ -184,11 +185,12 @@ class AppActivity : AppCompatActivity() {
         content: @Composable () -> Unit,
     ) = with(navigationHost) {
         val backProgress by registerPredictiveBackHandler(onBack = ::back)
+        val animatedBackProgress by animateFloatAsState(targetValue = backProgress, label = "OverlayBackProgress")
 
         Box(
             content = { content() },
             modifier = Modifier.graphicsLayer {
-                alpha = 1F - backProgress
+                alpha = 1F - animatedBackProgress
             }
         )
     }

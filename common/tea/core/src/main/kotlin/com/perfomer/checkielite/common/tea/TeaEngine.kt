d428582acc26635ba78f3055d1ef2d7692e61748
Denis Balchugov <perfomeria@gmail.com>
2025-04-01 23:59:11 +0700

    Fix whitespaces for TeaEngine file

--- common/tea/core/src/main/kotlin/com/perfomer/checkielite/common/tea/TeaEngine.kt
+++ common/tea/core/src/main/kotlin/com/perfomer/checkielite/common/tea/TeaEngine.kt
@@ -32,81 +32,81 @@ import kotlinx.coroutines.launch
  * - [UiState] - UI layer state
  */
 internal class TeaEngine<Command : Any, Effect : Any, Event : Any, UiEvent : Event, State : Any, UiState : Any>(
-	internal val initialState: State,
-	private val initialEvents: List<Event> = emptyList(),
-	private val reducer: Reducer<Command, Effect, Event, State>,
-	private val uiStateMapper: UiStateMapper<State, UiState>? = null,
-	private val actor: Actor<Command, Event>,
-	// TODO: Add UnhandledExceptionHandler for Store
+    internal val initialState: State,
+    private val initialEvents: List<Event> = emptyList(),
+    private val reducer: Reducer<Command, Effect, Event, State>,
+    private val uiStateMapper: UiStateMapper<State, UiState>? = null,
+    private val actor: Actor<Command, Event>,
+    // TODO: Add UnhandledExceptionHandler for Store
 ) : Store<Effect, UiEvent, UiState> {
 
-	override val effects: Flow<Effect>
-		get() = effectsChannel.receiveAsFlow()
+    override val effects: Flow<Effect>
+        get() = effectsChannel.receiveAsFlow()
 
-	override val state: StateFlow<UiState>
-		get() = uiStateFlow.asStateFlow()
+    override val state: StateFlow<UiState>
+        get() = uiStateFlow.asStateFlow()
 
-	private lateinit var storeScope: CoroutineScope
+    private lateinit var storeScope: CoroutineScope
 
-	internal val stateFlow = MutableStateFlow(initialState)
+    internal val stateFlow = MutableStateFlow(initialState)
 
-	private val uiStateFlow = MutableStateFlow(initialState.toUiState())
-	private val commandsFlow = MutableSharedFlow<Command>(replay = 1)
-	private val effectsChannel = Channel<Effect>(Channel.BUFFERED)
-	private val eventsFlow = MutableSharedFlow<Event>(replay = 1)
+    private val uiStateFlow = MutableStateFlow(initialState.toUiState())
+    private val commandsFlow = MutableSharedFlow<Command>(replay = 1)
+    private val effectsChannel = Channel<Effect>(Channel.BUFFERED)
+    private val eventsFlow = MutableSharedFlow<Event>(replay = 1)
 
-	fun launch(scope: CoroutineScope) {
-		storeScope = scope
+    fun launch(scope: CoroutineScope) {
+        storeScope = scope
 
-		setupStateFlow()
-		setupCommandsFlow(actor)
-		setupEventsFlow(reducer)
+        setupStateFlow()
+        setupCommandsFlow(actor)
+        setupEventsFlow(reducer)
 
-		applyInitialEvents(initialEvents)
-	}
+        applyInitialEvents(initialEvents)
+    }
 
-	override fun accept(event: UiEvent) {
-		checkLaunch()
-		storeScope.launch { eventsFlow.emit(event) }
-	}
+    override fun accept(event: UiEvent) {
+        checkLaunch()
+        storeScope.launch { eventsFlow.emit(event) }
+    }
 
-	private fun setupStateFlow() {
-		stateFlow
-			.map { it.toUiState() }
-			.onEach(uiStateFlow::emit)
-			.launchIn(scope = storeScope)
-	}
+    private fun setupStateFlow() {
+        stateFlow
+            .map { it.toUiState() }
+            .onEach(uiStateFlow::emit)
+            .launchIn(scope = storeScope)
+    }
 
-	private fun setupCommandsFlow(actor: Actor<Command, Event>) {
-		actor.act(commandsFlow)
-			.onEach(eventsFlow::emit)
-			.launchIn(scope = storeScope)
-	}
+    private fun setupCommandsFlow(actor: Actor<Command, Event>) {
+        actor.act(commandsFlow)
+            .onEach(eventsFlow::emit)
+            .launchIn(scope = storeScope)
+    }
 
-	private fun setupEventsFlow(reducer: Reducer<Command, Effect, Event, State>) {
-		eventsFlow
-			.map { event -> reducer.reduce(stateFlow.value, event) }
-			.onEach { update -> update.state?.let { stateFlow.emit(it) } }
-			.onEach { update -> update.commands.forEach { commandsFlow.emit(it) } }
-			.onEach { update -> update.effects.forEach { effectsChannel.send(it) } }
-			.launchIn(scope = storeScope)
-	}
+    private fun setupEventsFlow(reducer: Reducer<Command, Effect, Event, State>) {
+        eventsFlow
+            .map { event -> reducer.reduce(stateFlow.value, event) }
+            .onEach { update -> update.state?.let { stateFlow.emit(it) } }
+            .onEach { update -> update.commands.forEach { commandsFlow.emit(it) } }
+            .onEach { update -> update.effects.forEach { effectsChannel.send(it) } }
+            .launchIn(scope = storeScope)
+    }
 
-	private fun applyInitialEvents(initialEvents: List<Event>) {
-		if (initialEvents.isEmpty()) return
+    private fun applyInitialEvents(initialEvents: List<Event>) {
+        if (initialEvents.isEmpty()) return
 
-		storeScope.launch {
-			eventsFlow.emitAll(initialEvents.asFlow())
-		}
-	}
+        storeScope.launch {
+            eventsFlow.emitAll(initialEvents.asFlow())
+        }
+    }
 
-	private fun checkLaunch() {
-		if (::storeScope.isInitialized) return
-		error("TeaEngine hasn't launched. Did you call launch()?")
-	}
+    private fun checkLaunch() {
+        if (::storeScope.isInitialized) return
+        error("TeaEngine hasn't launched. Did you call launch()?")
+    }
 
-	@Suppress("UNCHECKED_CAST")
-	private fun State.toUiState(): UiState {
-		return uiStateMapper?.map(this) ?: this as UiState
-	}
+    @Suppress("UNCHECKED_CAST")
+    private fun State.toUiState(): UiState {
+        return uiStateMapper?.map(this) ?: this as UiState
+    }
 }

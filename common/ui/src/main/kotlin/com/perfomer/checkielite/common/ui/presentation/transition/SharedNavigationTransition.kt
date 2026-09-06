cf3c65e74bc638cb63d6b342b412c62d7e422d83
Denis Balchugov <perfomeria@gmail.com>
2026-09-07 00:15:28 +0700

    Change transition duration

--- common/ui/src/main/kotlin/com/perfomer/checkielite/common/ui/presentation/transition/SharedNavigationTransition.kt
+++ common/ui/src/main/kotlin/com/perfomer/checkielite/common/ui/presentation/transition/SharedNavigationTransition.kt
@@ -4,7 +4,7 @@ import androidx.compose.animation.AnimatedVisibilityScope
 import androidx.compose.animation.SharedTransitionScope
 import androidx.compose.runtime.compositionLocalOf
 
-const val SharedNavigationTransitionDurationMillis: Int = 250
+const val SharedNavigationTransitionDurationMillis: Int = 300
 
 val LocalSharedTransitionScope = compositionLocalOf<SharedTransitionScope?> { null }
 

8d0bea2f665049c2c56c1a4a3dd0a432ca3bd274
Denis Balchugov <perfomeria@gmail.com>
2026-09-13 02:47:56 +0700

    Fix predictive back handling for modal bottom sheets

--- common/ui/build.gradle.kts
+++ common/ui/build.gradle.kts
@@ -21,6 +21,7 @@ dependencies {
 	api(libs.androidx.compose.animation)
 	implementation(libs.accompanist.systemuicontroller)
 	implementation(libs.androidx.activity.compose)
+	implementation(libs.androidx.navigationevent.compose)
 	implementation(libs.androidx.appcompat)
 	implementation(libs.androidx.compose.material3)
 	implementation(libs.androidx.compose.ui.tooling)
